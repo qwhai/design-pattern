3b983400439d03e7a5fa01bf968a4828a35bd0a5
Q-WHai <return_zero0@163.com>
2019-06-13 19:11:08 +0800

    feat:「Visitor」

---
+++ src/main/java/pers/hai/sample/dp/visitor/readme.md
@@ -0,0 +1,13 @@
+## Visitor Design Pattern.
+
+> 访问者模式：表示一个作用于某对象结构中的各元素的操作。它使你可以在不改变各元素的类的前提下定义作用于这些元素的新操作。
+
+### Ref
+- 《图解设计模式》
+
+-----
+
+- [CSDN](https://qwhai.blog.csdn.net/)
+- [Design pattern special column](http://blog.csdn.net/column/details/java-designpattern-w.html)
+- [Github](https://github.com/qwhai)
+- [E-mail](return_zero0@163.com)
---
+++ src/test/java/pers/hai/sample/dp/VisitorTests.java
@@ -0,0 +1,21 @@
+package pers.hai.sample.dp;
+
+import org.junit.Test;
+
+/**
+ * TODO
+ * <p>
+ * Create Time: 2019-06-13 18:40
+ * Last Modify: 2019-06-13
+ *
+ * @author Q-WHai
+ * @see <a href="https://github.com/qwhai">https://github.com/qwhai</a>
+ */
+public class VisitorTests extends Testable {
+
+    @Test
+    public void test1() {
+        // TODO
+    }
+
+}
